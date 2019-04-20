b881b57a0da5a770772c104981e4bd1b7761a020
Nilanjan Siromani <nilanjan.siromani@walmartlabs.com>
2019-04-20 20:07:23 +0530

    Add promise handlers

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const config = require("./config/launch-config").config;
 const liveViewHelper = require("./network-warper");
-
+const chalk = require("chalk");
 
 const liveViewDeskTop = async browser => {
   const page = await browser.newPage();
-  page.setViewport(config.viewport);
+  await page.setViewport(config.viewport);
   await page.setRequestInterception(true);
   page.on("request", interceptedRequest => {
-    liveViewHelper.networkWarper(interceptedRequest);
+    liveViewHelper.networkWarper(interceptedRequest).catch((e)=>{
+      console.log(chalk.bgRed.yellow.bold("OOPS something went wrong"));
+    });
   });
-  await page.goto(config.url);
+
+  await page.goto(config.url)
 };
 
 const liveView = async () => {
   const browser = await puppeteer.launch(config.launchConfig);
-  liveViewDeskTop(browser);
+  await liveViewDeskTop(browser);
 };
 
 
-liveView();
+liveView().catch((e)=>{
+  console.log(chalk.bgRed.yellow.bold("OOPS something went wrong"));
+});
