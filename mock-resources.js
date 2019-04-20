59a9d45e46178210d5b592c51e4fb409b30f25fb
Nilanjan Siromani <nilanjan.siromani@walmartlabs.com>
2019-04-20 12:16:00 +0530

    Add a mock rerource

---
+++ mock-resources.js
@@ -0,0 +1,20 @@
+exports.mocks = {
+    "https://i5.wal.co/dfw/63fd9f59-e4a9/5d90040b-09e1-49f3-90ca-4affa4b29196/v1/fitment_widget_JS.ITEM_PAGE-2.5.0.bundle.550d862006545523f5d4.js" : {
+        respondWith : {
+            contentType: "text/javascript",
+            body: "./mock-responses/foo.js"
+        }
+    },
+    "fitment_widget_CSS" : {
+        respondWith : {
+            contentType: "text/css",
+            body: "./mock-responses/foo.css"
+        }
+    },
+    "config" : {
+        respondWith : {
+            contentType: "text/css",
+            body: "./mock-responses/foo.css"
+        }
+    }
+}
