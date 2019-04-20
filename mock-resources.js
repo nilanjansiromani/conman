662de76703fac66781a9ea1e169b269bcddde618
Nilanjan Siromani <nilanjan.siromani@walmartlabs.com>
2019-04-20 12:20:02 +0530

    final touches

--- mock-resources.js
+++ mock-resources.js
@@ -5,7 +5,7 @@ exports.mocks = {
             body: "./mock-responses/foo.js"
         }
     },
-    "fitment_widget_CSS" : {
+    "https://i5.wal.co/dfw/63fd9f59-e4a9/5d90040b-09e1-49f3-90ca-4affa4b29196/v1/fitment_widget_CSS.ITEM_PAGE-2.5.0.style.550d862006545523f5d4.css" : {
         respondWith : {
             contentType: "text/css",
             body: "./mock-responses/foo.css"
