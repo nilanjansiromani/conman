9d93fb6746f61f9e872c2a57a35d393330781f8d
Nilanjan Siromani <nilanjan.siromani@walmartlabs.com>
2019-04-20 20:08:17 +0530

    Complete urls not neccessary for mocking - just a small part will do

--- network-warper.js
+++ network-warper.js
@@ -1,20 +1,15 @@
-const chalk = require("chalk");
 const mocks = require("./mock-resources").mocks;
-const log = console.log;
 const fs = require("fs");
 
 const networkWarper = async interceptedRequest => {
-  const mockUrlList = Object.keys(mocks);
-  if (mockUrlList.includes(interceptedRequest.url())) {
-    const mockUrL = interceptedRequest.url();
-    log(chalk.green(" [c]  conman just mokcked " + interceptedRequest.url()));
-    const resposeObject = mocks[mockUrL]["respondWith"];
-    console.log(resposeObject);
-    interceptedRequest.respond({
-      contentType: resposeObject.contentType,
-      body: fs.readFileSync(resposeObject.body)
-    });
-  }
+  Object.keys(mocks).forEach((pattern)=>{
+    if(interceptedRequest.url().includes(pattern)){
+      interceptedRequest.respond({
+        contentType: mocks[pattern].respondWith.contentType,
+        body: fs.readFileSync(mocks[pattern].respondWith.body)
+      });
+    }
+  });
   interceptedRequest.continue();
 };
 
